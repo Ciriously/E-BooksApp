00edd078dba3a4c7462eab9932e51299d7330e1b
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-09-03 13:06:57 +0530

    CoverScreen

--- Login.js
+++ Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import Home from './Home'; // Import the Home component
 
 const LoginPage = ({ navigation }) => {
@@ -10,12 +10,23 @@ const LoginPage = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login Page</Text>
+      {/* Pink Background Container */}
+      <View style={styles.pinkContainer}>
+        <Text style={styles.titleText}>Title Text</Text>
 
-      {/* Green right arrow */}
-      <TouchableOpacity style={styles.greenArrow} onPress={handleGoToHome}>
-        <Text style={styles.arrowText}>➜</Text>
-      </TouchableOpacity>
+        {/* Circular arrow inside the pink container */}
+        <TouchableOpacity style={styles.greenArrow} onPress={handleGoToHome}>
+          <Image source={require('./assets/right-arrow.png')} style={styles.arrowImage} />
+        </TouchableOpacity>
+      </View>
+
+      {/* Background Image */}
+      <View style={styles.backgroundContainer}>
+        <Image
+          source={require('./assets/Images/cover.jpg')}
+          style={styles.backgroundImage}
+        />
+      </View>
     </View>
   );
 };
@@ -23,22 +34,42 @@ const LoginPage = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-end', // Align items to the bottom
+  },
+  pinkContainer: {
+    flex: 1,
+    backgroundColor: 'pink', // Pink background color
     justifyContent: 'center',
     alignItems: 'center',
   },
-  title: {
+  titleText: {
     fontSize: 24,
-    marginBottom: 20,
+    fontWeight: 'bold',
+    color: 'white', // Text color
   },
   greenArrow: {
     backgroundColor: 'green',
-    padding: 20,
-    borderRadius: 50, // Makes it circular
+    width: 50,
+    height: 50,
+    borderRadius: 25, // Makes it circular
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10, // Adjust the space between the title text and the arrow
   },
-  arrowText: {
-    color: 'white',
-    fontSize: 36,
-    fontWeight: 'bold',
+  arrowImage: {
+    width: 20,
+    height: 20,
+  },
+  backgroundContainer: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'flex-end', // Align the image to the bottom
+  },
+  backgroundImage: {
+    width: '100%',
+    height: 500, // Adjust the height as needed to minimize the image
+    resizeMode: 'cover', // Maintain aspect ratio and cover the container
+    bottom: -90, // Position the image at the very bottom of the container
   },
 });
 
