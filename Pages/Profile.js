83c67e69286050e1e1d2f328a5a24e3bc3813a6a
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-09-07 17:12:48 +0530

    created a profile page

--- Pages/Profile.js
+++ Pages/Profile.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { useFonts } from 'expo-font';
 
-const App = () => {
+const Profile = () => {
+  const [fontsLoaded] = useFonts({
+    'Gordita-Regular': require('../assets/fonts/Gordita-Regular.ttf'),
+    'Gordita-Bold': require('../assets/fonts/Gordita-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      {/* Cover Picture */}
+      <View style={styles.coverPic}>
+        <Image
+          source={require('../assets/cover.jpg')}
+          style={styles.coverImage}
+        />
+      </View>
+
+      <View style={styles.card}>
+        {/* Profile Icon */}
+        <View style={styles.profileIconContainer}>
+          <Image
+            source={require('../assets/icon2.png')}
+            style={styles.profileIcon}
+          />
+        </View>
+
+        {/* User Details */}
+        <View style={styles.userDetails}>
+          <Text style={styles.userName}>Guddu</Text>
+          <Text style={styles.userBio}>
+            Gudiya's Guddu aka Aditya Mishra loves Reading books
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -12,9 +46,58 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  coverPic: {
+    height: 200, // Adjust the height as needed
+    backgroundColor: '#007AFF',
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  coverImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 30,
+    padding: 20,
+    shadowColor: '#007AFF', // Light blue shadow color
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.5, // Adjust the opacity as needed
+    shadowRadius: 4,
+    elevation: 5,
+    marginHorizontal: 20,
+    marginTop: -50, // Adjust the margin-top to overlap with the cover pic
+    alignItems: 'center',
+  },
+  profileIconContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 75,
+    width: 150,
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  profileIcon: {
+    width: 120,
+    height: 120,
+    resizeMode: 'contain',
+  },
+  userName: {
+    fontFamily: 'Gordita-Bold',
+    fontSize: 24,
+    marginBottom: 10,
+    marginLeft: 100,
+  },
+  userBio: {
+    fontFamily: 'Gordita-Regular',
+    fontSize: 16,
+    textAlign: 'center',
+    paddingHorizontal: 20,
   },
 });
 
-export default App;
+export default Profile;
