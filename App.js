edaa4037b456df9a276080a02437bd6ecb9c11b0
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-11-15 17:30:54 +0530

    added stacknavigator

--- App.js
+++ App.js
@@ -1,52 +1,9 @@
+// App.js
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Onboarding from "./Pages/Onboarding";
-import Home from "./Pages/Home";
-import Profile from "./Pages/Profile";
-import Bookmarks from "./Pages/library";
-import Reader from "./Reader";
-import Footer from "./Footer";
-import Login from "./Login"; // Import the Login screen
+import AppNavigator from "./AppNavigator";
 
-const Stack = createStackNavigator();
+const App = () => {
+  return <AppNavigator />;
+};
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Onboarding">
-        <Stack.Screen
-          name="Onboarding"
-          component={Onboarding}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Bookmarks"
-          component={Bookmarks}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Reader"
-          component={Reader}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Login" // Add the 'Login' screen
-          component={Login}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-      <Footer />
-    </NavigationContainer>
-  );
-}
+export default App;
--- AppNavigator.js
+++ AppNavigator.js
@@ -1,25 +1,61 @@
 // AppNavigator.js
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginPage from './Login';
-import Home from './Pages/Home';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Onboarding from "./Pages/Onboarding";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Bookmarks from "./Pages/library";
+import Reader from "./Reader";
+import Footer from "./Footer";
+import Login from "./Pages/Login";
+import NewBookInfo from "./Components/NewBookInfo";
 
 const Stack = createStackNavigator();
 
-export default function AppNavigator() {
+const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="LoginPage">
-      <Stack.Screen
-        name="Login"
-        component={LoginPage}
-        options={{ headerShown: false }}
-        initialParams={{ showFooter: false }}
-      />
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Onboarding"
+          component={Onboarding}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NewBookInfo"
+          component={NewBookInfo}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Bookmarks"
+          component={Bookmarks}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Reader"
+          component={Reader}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+      <Footer />
+    </NavigationContainer>
   );
-}
+};
+
+export default AppNavigator;
