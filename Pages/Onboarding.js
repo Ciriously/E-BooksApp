f20b2a0e4fbe339bca27c660510f9e0d2205023f
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2023-11-15 14:34:55 +0530

    finished the onboarding page

--- Pages/Onboarding.js
+++ Pages/Onboarding.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import Swiper from "react-native-swiper";
-import Gif from "react-native-gif";
+import * as Font from "expo-font";
+
+// Load custom fonts
+Font.loadAsync({
+  "Gordita-Regular": require("../assets/fonts/Gordita-Regular.ttf"),
+  "Gordita-Bold": require("../assets/fonts/Gordita-Bold.ttf"),
+  "EBGaramond-Italic": require("../assets/fonts/EBGaramond-Italic.ttf"),
+  "EBGaramond-Medium": require("../assets/fonts/EBGaramond-Medium.ttf"),
+});
 
 const Onboarding = ({ navigation }) => {
   const handleFinish = () => {
@@ -9,32 +17,57 @@ const Onboarding = ({ navigation }) => {
     navigation.navigate("Home");
   };
 
+  const handleSkip = () => {
+    // Navigate to the next screen (e.g., Home) directly
+    navigation.navigate("Home");
+  };
+
   return (
     <Swiper loop={false} showsPagination={true}>
       {/* Slide 1 */}
       <View style={styles.slide}>
-        <Gif
-          style={styles.gif}
-          source={require("./path_to_your_car.gif")} // Replace with your GIF source
-          resizeMode="cover"
+        <Image
+          source={require("../assets/slide1.png")}
+          style={styles.backgroundImage}
         />
-        <Text style={styles.title}>Welcome to Onboarding</Text>
-        <Text style={styles.description}>This is slide 1</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>
+            “A reader lives a thousand lives before he dies . . . The man who
+            never reads lives only one.”
+          </Text>
+          <Text style={styles.description}>-George R.R. Martin</Text>
+        </View>
       </View>
 
       {/* Slide 2 */}
       <View style={styles.slide}>
-        <Text style={styles.title}>Explore Features</Text>
-        <Text style={styles.description}>This is slide 2</Text>
+        <Image
+          source={require("../assets/slide2.jpg")}
+          style={styles.backgroundImage}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.title2}>Explore Features</Text>
+          <Text style={styles.description2}>This is slide 2</Text>
+        </View>
       </View>
 
       {/* Slide 3 */}
       <View style={styles.slide}>
-        <Text style={styles.title}>Get Started</Text>
-        <Text style={styles.description}>This is slide 3</Text>
-        <Text style={styles.finishButton} onPress={handleFinish}>
-          Finish
-        </Text>
+        <Image
+          source={require("../assets/slide3.jpg")}
+          style={styles.backgroundImage}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.title3}>Get Started</Text>
+          <Text style={styles.description3}>This is slide 3</Text>
+          <TouchableOpacity style={styles.finishButton} onPress={handleFinish}>
+            <Text>Finish</Text>
+          </TouchableOpacity>
+          {/* Skip Button inside the third screen */}
+          <TouchableOpacity style={styles.skipButton} onPress={handleSkip}>
+            <Text style={styles.skipButtonText}>Skip</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </Swiper>
   );
@@ -43,28 +76,68 @@ const Onboarding = ({ navigation }) => {
 const styles = StyleSheet.create({
   slide: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    justifyContent: "center",
   },
-  gif: {
-    width: 200,
-    height: 200,
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  textContainer: {
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.0)",
+    padding: 20,
+    borderRadius: 10,
+    top: "30%",
   },
   title: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontFamily: "Gordita-Bold",
     marginBottom: 20,
+    color: "#fff",
   },
   description: {
     fontSize: 16,
     textAlign: "center",
+    fontFamily: "Gordita-Regular",
+    color: "#fff",
+  },
+  title2: {
+    fontSize: 24,
+    fontFamily: "Gordita-Bold",
+    marginBottom: 20,
+    color: "#fff",
+  },
+  description2: {
+    fontSize: 16,
+    textAlign: "center",
+    fontFamily: "Gordita-Regular",
+    color: "#fff",
+  },
+  title3: {
+    // Add styles for slide 3 title
+  },
+  description3: {
+    // Add styles for slide 3 description
   },
   finishButton: {
     marginTop: 20,
     fontSize: 18,
     color: "blue",
     textDecorationLine: "underline",
+  },
+  skipButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    padding: 10,
+    borderRadius: 5,
+    zIndex: 1,
+  },
+  skipButtonText: {
+    color: "#000",
   },
 });
 
